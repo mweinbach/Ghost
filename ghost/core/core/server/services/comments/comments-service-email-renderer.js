55c72ea92a8f493f4baf9106b1b87c8eaabb5689
mweinbach <maxweinbach5@gmail.com>
2026-06-28 19:37:18 -0400

    Improved comments email rendering by memoizing templates
    no ref
    
    - comments-service-email-renderer re-read and re-compiled the Handlebars template from disk on every renderEmailTemplate() call
    - templates are fixed at build time, so cache the compiled template per name for the process lifetime
    - also drop a redundant Buffer.from(...).toString() round-trip (readFile with 'utf8' already returns a string)

--- ghost/core/core/server/services/comments/comments-service-email-renderer.js
+++ ghost/core/core/server/services/comments/comments-service-email-renderer.js
@@ -4,6 +4,9 @@ const path = require('path');
 class CommentsServiceEmailRenderer {
     constructor({t}) {
         this.t = t;
+        // Compiled templates are fixed at build time, so cache them for the
+        // lifetime of the process instead of re-reading/compiling on every render.
+        this.compiledTemplates = new Map();
 
         this.Handlebars = require('handlebars').create();
         this.Handlebars.registerHelper('t', function (key, options) {
@@ -22,8 +25,12 @@ class CommentsServiceEmailRenderer {
     }
 
     async renderEmailTemplate(templateName, data) {
-        const htmlTemplateSource = await fs.readFile(path.join(__dirname, './email-templates/', `${templateName}.hbs`), 'utf8');
-        const htmlTemplate = this.Handlebars.compile(Buffer.from(htmlTemplateSource).toString());
+        let htmlTemplate = this.compiledTemplates.get(templateName);
+        if (!htmlTemplate) {
+            const htmlTemplateSource = await fs.readFile(path.join(__dirname, './email-templates/', `${templateName}.hbs`), 'utf8');
+            htmlTemplate = this.Handlebars.compile(htmlTemplateSource);
+            this.compiledTemplates.set(templateName, htmlTemplate);
+        }
         const {renderText} = require(`./email-templates/${templateName}.txt`);
 
         const html = htmlTemplate(data);
